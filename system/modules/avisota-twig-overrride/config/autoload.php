b8c86647ef1a6088f24eec88671b07adbf962c80
Sven Baumann <baumann.sv@gmail.com>
2015-03-13 13:56:54 +0100

    create without composer version

---
+++ system/modules/avisota-twig-overrride/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package Avisota_twig_overrride
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Classes
+	'AvisotaTwigOverride' => 'system/modules/avisota_twig_overrride/classes/AvisotaTwigOverride.php',
+));
